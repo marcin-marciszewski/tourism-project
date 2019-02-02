066f80db127ad431481eda81d3e52a30b4b70b6c
Marcin Marciszewski <marciszewski.m@gmail.com>
2019-02-02 06:39:41 +0000

    Cards with attractions, hotels and restaurants have been added to the front page

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
-$(document).ready(function() {
- $('.landing-text h1').mouseenter(function() {
+
+ 
+ // The idea for this part of the code was taken from: https://www.youtube.com/watch?v=V9CY0F4Wc7M
+ 
+ $(document).ready(function() {
+  $('h3').fadeOut(10);
+
+ $(window).scroll(function() {
+  const scrolled = Math.floor(window.scrollY / 100);
+  console.log(scrolled);
+ 
+  if (scrolled >=5) {
+     $('.first').fadeIn(1000);
+  } 
+  
+  if (scrolled >= 23) {
+     $('.second').fadeIn(1000);
+  } 
+  
+    if (scrolled >= 43) {
+     $('.third').fadeIn(1000);
+  } 
+  
+    if (scrolled >= 63) {
+     $('.fourth').fadeIn(1000);
+  } 
+  
+    if (scrolled >= 81) {
+     $('.fifth').fadeIn(1000);
+  } 
+  
+    if (scrolled >= 99) {
+     $('.sixth').fadeIn(1000);
+  } 
+  
+});
+
+  $('.landing-text h1').mouseenter(function() {
 
   var col = ("rgb(" + Math.floor(Math.random() * 255) +
    "," + Math.floor(Math.random() * 255) + "," +
@@ -14,12 +50,20 @@ $(document).ready(function() {
   $(this).css('font-size', '12vw');
  });
 
- $('.centered').mouseenter(function() {
-  $(this).css('font-family', 'Roboto');
+ $('h4').mouseenter(function() {
+   var col = ("rgb(" + Math.floor(Math.random() * 255) +
+   "," + Math.floor(Math.random() * 255) + "," +
+   Math.floor(Math.random() * 255) + ")");
+  $(this).css('color', col);
+  $(this).css('font-family', 'Righteous')
  });
- $('.centered').mouseleave(function() {
-  $(this).css('font-family', 'Bungee');
+ $('h4').mouseleave(function() {
+   var col = ("rgb(" + Math.floor(Math.random() * 255) +
+   "," + Math.floor(Math.random() * 255) + "," +
+   Math.floor(Math.random() * 255) + ")");
+  $(this).css('color', col);
+   $(this).css('font-family', 'Bungee')
  });
- 
- 
+
+
 });
