6903f11e11f54d4cfea7ef24b09286e9a6172fbb
Marcin Marciszewski <marciszewski.m@gmail.com>
2019-02-02 07:35:26 +0000

    Function in the app.js has been added to allow text in the cards appear when scrolling

--- app.js
+++ app.js
@@ -1,69 +1,59 @@
-
- 
- // The idea for this part of the code was taken from: https://www.youtube.com/watch?v=V9CY0F4Wc7M
- 
  $(document).ready(function() {
   $('h3').fadeOut(10);
 
- $(window).scroll(function() {
-  const scrolled = Math.floor(window.scrollY / 100);
-  console.log(scrolled);
- 
-  if (scrolled >=5) {
-     $('.first').fadeIn(1000);
-  } 
-  
-  if (scrolled >= 23) {
-     $('.second').fadeIn(1000);
-  } 
-  
-    if (scrolled >= 43) {
-     $('.third').fadeIn(1000);
-  } 
-  
-    if (scrolled >= 63) {
-     $('.fourth').fadeIn(1000);
-  } 
-  
-    if (scrolled >= 81) {
-     $('.fifth').fadeIn(1000);
-  } 
-  
-    if (scrolled >= 99) {
-     $('.sixth').fadeIn(1000);
-  } 
-  
-});
+  $('.first').appear(function() {
+   $('.first-show').fadeIn(1500);
+  });
+
+  $('.second').appear(function() {
+   $('.second-show').fadeIn(1500);
+  });
+
+  $('.third').appear(function() {
+   $('.third-show').fadeIn(1500);
+  });
+
+  $('.fourth').appear(function() {
+   $('.fourth-show').fadeIn(1500);
+  });
+
+  $('.fifth').appear(function() {
+   $('.fifth-show').fadeIn(1500);
+  });
+
+  $('.sixth').appear(function() {
+   $('.sixth-show').fadeIn(1500);
+  });
 
   $('.landing-text h1').mouseenter(function() {
 
-  var col = ("rgb(" + Math.floor(Math.random() * 255) +
-   "," + Math.floor(Math.random() * 255) + "," +
-   Math.floor(Math.random() * 255) + ")");
-
-
-  $(this).css('color', col);
-  $(this).css('font-size', '14vw');
- });
- $('.landing-text h1').mouseleave(function() {
-  $(this).css('color', 'white');
-  $(this).css('font-size', '12vw');
- });
-
- $('h4').mouseenter(function() {
    var col = ("rgb(" + Math.floor(Math.random() * 255) +
-   "," + Math.floor(Math.random() * 255) + "," +
-   Math.floor(Math.random() * 255) + ")");
-  $(this).css('color', col);
-  $(this).css('font-family', 'Righteous')
- });
- $('h4').mouseleave(function() {
+    "," + Math.floor(Math.random() * 255) + "," +
+    Math.floor(Math.random() * 255) + ")");
+
+
+   $(this).css('color', col);
+   $(this).css('font-size', '14vw');
+  });
+  $('.landing-text h1').mouseleave(function() {
+   $(this).css('color', 'white');
+   $(this).css('font-size', '12vw');
+  });
+
+  $('h4').mouseenter(function() {
    var col = ("rgb(" + Math.floor(Math.random() * 255) +
-   "," + Math.floor(Math.random() * 255) + "," +
-   Math.floor(Math.random() * 255) + ")");
-  $(this).css('color', col);
+    "," + Math.floor(Math.random() * 255) + "," +
+    Math.floor(Math.random() * 255) + ")");
+   $(this).css('color', col);
+   $(this).css('font-family', 'Righteous')
+  });
+  $('h4').mouseleave(function() {
+   var col = ("rgb(" + Math.floor(Math.random() * 255) +
+    "," + Math.floor(Math.random() * 255) + "," +
+    Math.floor(Math.random() * 255) + ")");
+   $(this).css('color', col);
    $(this).css('font-family', 'Bungee')
+  });
+  
  });
-
-
-});
+ 
